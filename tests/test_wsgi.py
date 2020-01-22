9f8a0ba6144251891614fbc22b1a8bfb804bcb4c
Riyad YAKINE <riyad.yakine@gmail.com>
2020-01-22 11:13:27 +0100

    Get product feature

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -11,3 +11,17 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_get_existing_product(self):
+        response = self.client.get("/api/v1/products/1")
+        product = response.json
+        status = response.status
+        self.assertIsInstance(product, dict)
+        self.assertEqual(status, 200)
+
+    def test_get_not_existing_product(self):
+        response = self.client.get("/api/v1/products/0")
+        product = response.json
+        status = response.status
+        self.assertIsInstance(product, None)
+        self.assertEqual(status, 404)
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 
 PRODUCTS = [
     {'id': 1, 'name': 'Skello' },
-    {'id': 2, 'name': 'Socialive.tv' }
+    {'id': 2, 'name': 'Socialive.tv' },
+    {'id': 3, 'name': 'lemonde.fr'}
 ]
 
 @app.route("/api/v1/products")
-def products():
+def get_product_list():
     return jsonify(PRODUCTS)
+
+@app.route("/api/v1/products/<int:id>")
+def get_product(id):
+    if id is in PRODUCTS:
+        product = PRODUCTS[id]
+        status = 200
+    else:
+        product = {}
+        status = 404
+    response = make_response(jsonify(product), 200)
+    return response
 
 @app.route('/')
 def hello():
