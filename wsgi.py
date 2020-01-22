5c74cab0c2b814164ccac56db6066190aa018a84
Riyad YAKINE <riyad.yakine@gmail.com>
2020-01-22 11:22:45 +0100

    Configure Travis CI

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
+# pylint: disable=missing-docstring
+
 from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 
 PRODUCTS = [
-    {'id': 1, 'name': 'Skello' },
-    {'id': 2, 'name': 'Socialive.tv' },
+    {'id': 1, 'name': 'Skello'},
+    {'id': 2, 'name': 'Socialive.tv'},
     {'id': 3, 'name': 'lemonde.fr'}
 ]
 
@@ -12,14 +14,14 @@ def get_product_list():
     return jsonify(PRODUCTS)
 
 @app.route("/api/v1/products/<int:id>")
-def get_product(id):
-    if id is in PRODUCTS:
-        product = PRODUCTS[id]
+def get_product(product_id):
+    if product_id in PRODUCTS:
+        product = PRODUCTS[product_id]
         status = 200
     else:
         product = {}
         status = 404
-    response = make_response(jsonify(product), 200)
+    response = make_response(jsonify(product), status)
     return response
 
 @app.route('/')
